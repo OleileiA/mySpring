a0e3b3c859f9588114aedfe765bb490b664e0034
OleileiA <972260032@qq.com>
2024-02-27 00:15:36 +0800

    AServiceimpl增加属性

--- src/main/java/com/mins/test/AServiceImpl.java
+++ src/main/java/com/mins/test/AServiceImpl.java
@@ -2,8 +2,33 @@ package com.mins.test;
 
 public class AServiceImpl implements AService {
 
+    private String name;
+
+    private int level;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
     @Override
     public void sayHello() {
-        System.out.println("Hello");
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public AServiceImpl(String name, int level) {
+        this.name = name;
+        this.level = level;
     }
 }
